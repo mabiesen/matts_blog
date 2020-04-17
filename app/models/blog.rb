79119c20f6f219d5ccc7668ad098373e918e9c9a
Matthew Biesen <mabiesen@outlook.com>
2020-04-17 03:08:35 -0500

    add method, add db seed

--- app/models/blog.rb
+++ app/models/blog.rb
@@ -1,2 +1,7 @@
 class Blog < ApplicationRecord
+  
+  def title_url
+    "#{self.created_at.to_date}_#{self.title.split(' ').join('_')}"
+  end
+
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,5 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Blog.create([{ title: 'test blog1', summary: 'test', content_html: '<p>heyo<p>'}])
