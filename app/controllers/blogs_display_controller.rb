6ea20c4804f50965738d586b9f30816d81c851ff
Matthew Biesen <mabiesen@outlook.com>
2020-04-19 03:22:26 -0500

    spacing

--- app/controllers/blogs_display_controller.rb
+++ app/controllers/blogs_display_controller.rb
@@ -4,9 +4,13 @@ class BlogsDisplayController < ApplicationController
   def blog_page
   end
 
+  def blog_home
+    @blogs = Blog.all
+  end
+
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_blog
-      @blog = Blog.where(title_url: params[:blog_title]).first
+	    @blog = Blog.where(title_url: params[:blog_title].downcase).first
     end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,5 @@ Rails.application.routes.draw do
   resources :blogs
 
   get '/blog_page/:blog_title', to: 'blogs_display#blog_page'
+  get '/blog_home', to: 'blogs_display#blog_home'
 end
