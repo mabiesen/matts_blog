c2f406f1fb2cdbd57e97746cbf8be89e991b304c
Matthew Biesen <mabiesen@outlook.com>
2020-04-27 00:36:13 -0500

    blog renamed, functional

--- config/application.rb
+++ config/application.rb
@@ -19,7 +19,7 @@ require "sprockets/railtie"
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
-module MattsWebsite
+module MattsBlog
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.0
